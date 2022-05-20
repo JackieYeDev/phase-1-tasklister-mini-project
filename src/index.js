f54864cfb6fd8461d2907111694457bf46a23f1e
Jackie <jye0325@gmail.com>
2022-05-20 00:32:31 -0400

    Assignment submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.addEventListener("submit", function () {
+  const tasks = document.getElementById("tasks");
+  const input = document.querySelector('input#new-task-description');
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  const priority = document.querySelector('select#priority-selector').value
+
+  // Appends our new todos to the list
+  li.textContent = input.value;
+  li.className = priority;
+  li.setAttribute("contenteditable", true)
+  button.textContent = "X"
+  button.addEventListener("click", function(){
+    li.remove()
+  })
+
+  li.append(button);
+  tasks.append(li);
+
+  // Clears the input
+  input.value = ""
+  
+  // preventDefault() for submit button and event
+  event.preventDefault();
+})
